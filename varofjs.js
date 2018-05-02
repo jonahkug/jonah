ec1da18ec9a8e873d6c36978b32122f27e880472
kisoro <kisorojonah@gmail.com>
2018-05-02 17:00:51 +0300

    first commit

---
+++ varofjs.js
@@ -0,0 +1,48 @@
+var average;
+average = 2;
+//var average = 2;
+console.log(average);
+
+x = 1
+y = 3
+z = 4
+if ( x > 10){
+    console.log(x)
+}
+
+result = 5 + 5 * 10;
+console.log(result);
+
+result = (5+5)*10;
+console.log(result);
+
+if( x === y){
+    console.log("they are not identical twins")
+} else {
+    console.log("they are identical twins")
+}
+
+var year = 2003;
+var remainder = year % 4;
+
+console.log(remainder);
+
+var age = 0;
+while ( age < 11){
+    console.log(age);
+    age++
+}
+
+function startSession(){
+    console.log('session started');
+
+
+}
+
+function sum( x, y){
+    j = x+y;
+    return j;
+
+}
+paul = sum(34, 6);
+console.log(paul);
